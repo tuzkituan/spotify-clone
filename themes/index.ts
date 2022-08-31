68416bb99ff331cda250ff1d69aa00f13efff746
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-08-31 21:31:04 +0700

    switch to default dark mode

--- themes/index.ts
+++ themes/index.ts
@@ -1,16 +1,16 @@
 // theme/index.js
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react';
 
 // Global style overrides
-import colors from './colors'
-import styles from './styles'
+import colors from './colors';
+import styles from './styles';
 
 // Foundational style overrides
 // import borders from './foundations/borders'
 
 // Component style overrides
-import Button from './components/button'
-import Modal from './components/modal'
+import Button from './components/button';
+import Modal from './components/modal';
 
 const overrides = {
   styles,
@@ -27,9 +27,9 @@ const overrides = {
     body: `'Noto Sans', sans-serif`,
   },
   config: {
-    initialColorMode: 'light',
+    initialColorMode: 'dark',
     useSystemColorMode: false,
-  }
-}
+  },
+};
 
-export default extendTheme(overrides)
+export default extendTheme(overrides);
